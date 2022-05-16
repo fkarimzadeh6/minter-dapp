5a0aa1fde81d9f35761337623e74a3ee490d6f88
fkarimzadeh6 <45274645+fkarimzadeh6@users.noreply.github.com>
2022-05-16 14:20:46 -0400

    Updated website

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x0f911ed5E881Ffacf6a065aa7bAbE2B7c26134f9"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the VE Vision NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The WE Vision NFT Project is a decentralized, open-source project that aims to empower all women around the world. Together, WE can be the change.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
